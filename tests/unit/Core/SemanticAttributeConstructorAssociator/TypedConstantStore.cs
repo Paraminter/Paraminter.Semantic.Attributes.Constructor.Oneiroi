874d7712a69356cb2f013390b500a7bb5557bc43
ErikWe <erikwestberg7@gmail.com>
2024-07-28 18:14:45 +0200

    Remove some debug code

--- tests/unit/Core/SemanticAttributeConstructorAssociator/TypedConstantStore.cs
+++ tests/unit/Core/SemanticAttributeConstructorAssociator/TypedConstantStore.cs
@@ -24,12 +24,6 @@ internal static class TypedConstantStore
 
         var compilation = CompilationFactory.Create(source);
 
-#pragma warning disable IDE0059 // Unnecessary assignment of a value
-#pragma warning disable S1481 // Unused local variables should be removed
-        var diagnostics = compilation.GetDiagnostics();
-#pragma warning restore S1481 // Unused local variables should be removed
-#pragma warning restore IDE0059 // Unnecessary assignment of a value
-
         var type = compilation.GetTypeByMetadataName("Foo")!;
         var attribute = type.GetAttributes()[0];
 
