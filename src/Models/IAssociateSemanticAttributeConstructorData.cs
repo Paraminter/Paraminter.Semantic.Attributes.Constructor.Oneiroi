145322836775ff01963a3674d7ed74658cd5078c
ErikWe <erikwestberg7@gmail.com>
2024-08-06 15:05:35 +0200

    Upt dependencies to use marker-interfaces

--- src/Models/IAssociateSemanticAttributeConstructorData.cs
+++ src/Models/IAssociateSemanticAttributeConstructorData.cs
@@ -2,10 +2,13 @@
 
 using Microsoft.CodeAnalysis;
 
+using Paraminter.Associators.Models;
+
 using System.Collections.Generic;
 
 /// <summary>Represents data used to associate semantic attribute constructor arguments.</summary>
 public interface IAssociateSemanticAttributeConstructorData
+    : IAssociateArgumentsData
 {
     /// <summary>The attribute constructor parameters.</summary>
     public abstract IReadOnlyList<IParameterSymbol> Parameters { get; }
